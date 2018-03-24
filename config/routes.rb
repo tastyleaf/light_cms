6e14a7103fcb8b4a07e13c1a3765d5617027f7f1
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-24 16:11:38 +0800

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :spreadsheets  do
+    member do
+      get :tabs
+      get :range
+    end
+
+    collection do
+      get :js
+    end
+  end  
+
+
 end
