2d35ff67746c02d0a24d118fc76d97ba92e278a6
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-24 17:18:46 +0800

    build  create action

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -7,8 +7,35 @@ class SpreadsheetsController < ApplicationController
     @spreadsheet = Spreadsheet.new
   end
 
+  def create
+    @spreadsheet = Spreadsheet.new(spreadsheet_params)
+    @spreadsheet.spreadsheet_id =  @spreadsheet.spreadsheet_id.split('/')[5]
+    if @spreadsheet.save
+      redirect_to tabs_spreadsheet_path(@spreadsheet)
+    else
+      #add restriction for unvailid url 
+      render :new
+    end
+  end
+
+  def tabs
+    
+  end
+
+
   def show
     
+  end
+
+
+  private
+
+  def set_spreadsheet
+    @spreadsheet = Spreadsheet.find(params[:id])    
+  end
+
+  def spreadsheet_params
+    params.require(:spreadsheet).permit(:spreadsheet_id, :tab_id, :tab_name, :range, :link_name)
   end
 
 end
